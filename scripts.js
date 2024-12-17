8247f3963d951bb34b6911981724672704ee23c4
jao <jcmf0110@gmail.com>
2024-12-17 20:09:15 -0300

    Upando o projeto no Github
    - Criação de uma barra de navegação
    - Criação de um Slider de imagens

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let cont = 1;
+let radio = document.querySelector('.manual-btn');
+document.getElementById('radio1').checked = true;
+
+setInterval(() => {
+    proximaImg();
+}, 5000)
+
+function proximaImg(){
+    cont++;
+    if(cont > 3){
+        cont = 1;
+    }
+    document.getElementById('radio' + cont).checked = true;
+}
