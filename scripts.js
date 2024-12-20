c3569b15a3588a9b9ceaff0aa4810d9ffdf63a27
jao <jcmf0110@gmail.com>
2024-12-20 17:54:33 -0300

    Criação da aba Sobre
    imagem
    pequeno texto
    slider de avaliações

--- scripts.js
+++ scripts.js
@@ -13,3 +13,30 @@ function proximaImg(){
     }
     document.getElementById('radio' + cont).checked = true;
 }
+
+
+
+
+//Slider das avaliações
+const slider = document.querySelector('.container-img-ava');
+const slides = document.querySelectorAll('.container-img-ava img');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function updateSliderPosition() {
+    const slideWidth = slides[0].clientWidth;
+    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+btnNext.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateSliderPosition();
+});
+
+btnPrev.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    updateSliderPosition();
+});
+
